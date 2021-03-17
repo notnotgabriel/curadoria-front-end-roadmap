b76d98926b8989d9a083857dc09c7b6d98b7d22d
Gabriel Ribeiro <biel.ribeiro1990@gmail.com>
2021-03-17 12:18:54 -0300

    create styled BrowserIcon component

---
+++ components/Icon/BrowserIcon.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+import browserIcon from '../../public/images/icons/browser.svg';
+
+export default styled(browserIcon)`
+  fill: #a7a7a7;
+  width: 50px;
+`;
