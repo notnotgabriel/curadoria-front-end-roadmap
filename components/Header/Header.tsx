b47cf939fa2636e1dd000f147356433018867cc4
Gabriel Ribeiro <biel.ribeiro1990@gmail.com>
2021-04-15 14:31:32 -0300

    use tailwind classes on Header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,56 +1,29 @@
-import styled, { css } from 'styled-components';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-
-const StyledHeaderList = styled.ul`
-  display: flex;
-  justify-content: center;
-  padding: 0;
-`;
-
-const StyledLink = styled.a<{ active?: boolean }>`
-  text-transform: uppercase;
-  color: #a7b2b4;
-  padding: 10px;
-  transition: color ease 0.3s;
-  font-size: 1.5em;
-  cursor: pointer;
-
-  &:hover {
-    color: #d7c676;
-  }
-
-  ${(props) =>
-    props.active &&
-    css`
-      color: #d7c676;
-      border-bottom: 1px solid;
-    `}
-`;
-
-const StyledHeader = styled.header`
-  height: 60px;
-  padding: 1.5em 0;
-`;
+import classnames from 'classnames';
 
 const Header = ({ navLinks }) => {
   const { query } = useRouter();
   return (
-    <StyledHeader>
-      <nav>
-        <StyledHeaderList>
-          {navLinks.map(({ id, title }) => {
-            return (
-              <li key={id}>
-                <Link href={`/${id}`}>
-                  <StyledLink active={id === query?.id}>{title}</StyledLink>
-                </Link>
-              </li>
-            );
-          })}
-        </StyledHeaderList>
-      </nav>
-    </StyledHeader>
+    <nav className='h-15 p-4 bg-white'>
+      <ul className='flex flex-row justify-between uppercase'>
+        {navLinks.map(({ id, title }) => {
+          return (
+            <li key={id}>
+              <Link href={`/${id}`}>
+                <a
+                  className={classnames('p-1', {
+                    'border-b-2 border-red-600': id === query?.id,
+                  })}
+                >
+                  {title}
+                </a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
   );
 };
 
