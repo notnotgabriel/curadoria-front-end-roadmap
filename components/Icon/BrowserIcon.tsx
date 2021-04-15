a67fcdbacc1983f7f87b501c239fbe878b5c0ffe
Gabriel Ribeiro <biel.ribeiro1990@gmail.com>
2021-04-15 16:37:35 -0300

    delete unused components

--- components/Icon/BrowserIcon.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-import browserIcon from '../../public/images/icons/browser.svg';
-
-export default styled(browserIcon)`
-  fill: #a7a7a7;
-  width: 50px;
-`;
