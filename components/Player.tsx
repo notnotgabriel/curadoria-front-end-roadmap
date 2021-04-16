d53de92a77322eeef6e97e571d0714a4fe4889cb
Gabriel Ribeiro <biel.ribeiro1990@gmail.com>
2021-04-16 00:29:22 -0300

    automaticaly update height of iframe using aspect ratio

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState, useRef, useLayoutEffect } from 'react';
 
 interface IPlayerProps {
   contentSrc: string;
@@ -6,16 +6,35 @@ interface IPlayerProps {
 }
 
 const Player: FC<IPlayerProps> = ({ contentSrc, title }) => {
+  const iframeRef = useRef(null);
+  const [iframeHeight, setIframeHeight] = useState(720);
+
+  useLayoutEffect(() => {
+    function handleResize() {
+      const iframeWidth = iframeRef?.current?.getBoundingClientRect().width;
+      const newIframeheight = 0.56 * iframeWidth;
+      setIframeHeight(newIframeheight);
+    }
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <iframe
-      height='315'
-      src={contentSrc}
-      allowFullScreen
-      title={title}
-      frameBorder='0'
-      allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-      className='w-full'
-    ></iframe>
+    <div>
+      <iframe
+        ref={iframeRef}
+        height={iframeHeight}
+        src={contentSrc}
+        allowFullScreen
+        title={title}
+        frameBorder='0'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+        className='w-full'
+      ></iframe>
+      <p className='font-semibold p-4'>{title}</p>
+    </div>
   );
 };
 
