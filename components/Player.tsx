60200958dffba37b381a2d6f16284df6a6645cdc
Gabriel Ribeiro <biel.ribeiro1990@gmail.com>
2021-07-13 22:20:45 -0300

    use native img tag

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,5 +1,4 @@
-import { FC, useState, useRef, useLayoutEffect } from 'react';
-import Image from 'next/image';
+import { FC, useState, useRef, useLayoutEffect } from "react";
 
 interface IPlayerProps {
   contentSrc: string;
@@ -10,7 +9,7 @@ const Player: FC<IPlayerProps> = ({ contentSrc, title }) => {
   const iframeRef = useRef(null);
   const [[iframeWidth, iframeHeight], setIframeHeight] = useState([872, 720]);
 
-  const isVideoContent = contentSrc.includes('youtube');
+  const isVideoContent = contentSrc.includes("youtube");
 
   useLayoutEffect(() => {
     function handleResize() {
@@ -19,9 +18,9 @@ const Player: FC<IPlayerProps> = ({ contentSrc, title }) => {
       setIframeHeight([iframeWidth, newIframeheight]);
     }
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     handleResize();
-    return () => window.removeEventListener('resize', handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return (
@@ -33,33 +32,32 @@ const Player: FC<IPlayerProps> = ({ contentSrc, title }) => {
           src={contentSrc}
           allowFullScreen
           title={title}
-          frameBorder='0'
-          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-          className='w-full'
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          className="w-full"
         ></iframe>
       ) : (
         <a
           ref={iframeRef}
           href={contentSrc}
-          target='_blank'
-          className='block relative'
+          target="_blank"
+          className="block relative"
         >
-          <Image
-            src='/images/reading.jpg'
-            layout='responsive'
-            width={872}
-            height={490}
-            className='filter brightness-75'
+          <img
+            src="/images/reading.jpg"
+            width={`${iframeWidth}px`}
+            height={`${iframeHeight}px`}
+            className="filter brightness-75"
           />
 
-          <div className='bg-white absolute rounded-sm pt-2 pb-0 w-3/4 left-1/2 -translate-x-1/2 -top-4 text-center shadow'>
-            <p className='my-2'>Clique para acessar este conteúdo em texto</p>
-            <div className='w-full bg-red-500 h-1' />
+          <div className="bg-white absolute rounded-sm pt-2 pb-0 w-3/4 left-1/2 -translate-x-1/2 -top-4 text-center shadow">
+            <p className="my-2">Clique para acessar este conteúdo em texto</p>
+            <div className="w-full bg-red-500 h-1" />
           </div>
         </a>
       )}
 
-      <p className='font-semibold py-4'>{title}</p>
+      <p className="font-semibold py-4">{title}</p>
     </div>
   );
 };
